c6e9416838832e96cf7a543ffeef6376b012ea9e
jonat <jonat@137.125.155.241>
2021-09-09 14:24:24 -0400

    testing something out

--- CSC211Week2/src/main/java/com/mycompany/csc211week2/NewClass.java
+++ CSC211Week2/src/main/java/com/mycompany/csc211week2/NewClass.java
@@ -2,6 +2,16 @@ package com.mycompany.csc211week2;
 
 public class NewClass {
     public static void main(String[] args) {
-        System.out.println("test");
+        
+        int[] arr = new int[5];
+        
+        arr[0] = 5;
+        
+        arr[2] = 10;
+        
+        for(int i = 0; i < 5; i++)
+        {
+            System.out.println("Array element value: " + arr[i]);
+        }
     }
 }
